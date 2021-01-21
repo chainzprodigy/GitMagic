808d9a12a76c436b04db942b89c31b8b9f3da010
Alex Centorbi <alexcentorbi07@gmail.com>
2021-01-21 12:20:52 -0600

    Create arccvg.c

---
+++ sentences/arccvg.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int partbrSentence1()
+{
+  printf("The cat named leonard was a happy cat.\n");
+  return 1;
+}
+
+int partbrSentence2()
+{
+  printf("He wanted food but I had none\n");
+  return 1;
+}
+
+
+
